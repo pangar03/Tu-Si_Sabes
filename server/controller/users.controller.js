f2e3c0ac3a63fe665420b498ed2cb813beb9d6fd
Juan Silva <juanse909@hotmail.com>
2025-06-04 22:14:24 -0500

    fix: error due to non-asynchronization solved

--- server/controller/users.controller.js
+++ server/controller/users.controller.js
@@ -1,33 +1,34 @@
-const {getUsers, register, login} = require("../db/users.db");
+// server/controller/users.controller.js
+const { getUsers, register, login } = require("../db/users.db");
 
 const getUsersController = async (req, res) => {
-    const response = await getUsers();
-    res.send(response);
+  const response = await getUsers();
+  res.send(response);
 };
 
 const registerController = async (req, res) => {
-    const user = req.body;
+  const user = req.body;
 
-    if(user.password !== user.confirmPassword) {
-        return res.send({ code: 400, message: "Las contraseñas no coinciden" });
-    }
+  if (user.password !== user.confirmPassword) {
+    return res.send({ code: 400, message: "Las contraseñas no coinciden" });
+  }
 
-    const id = Date.now();
-    
-    const response = register({...user, id});
+  const id = Date.now();
 
-    res.send(response);
+  const response = await register({ ...user, id }); // Ya está correcto con await
+
+  res.send(response);
 };
 
 const loginController = async (req, res) => {
-    const user = req.body;
-    const response = login(user);
+  const user = req.body;
+  const response = await login(user); // AGREGAR await aquí
 
-    res.send(response);
+  res.send(response);
 };
 
 module.exports = {
-    getUsersController,
-    registerController,
-    loginController,
-}
+  getUsersController,
+  registerController,
+  loginController,
+};
